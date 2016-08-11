350278f9d30e0f74e5a667bc42cf388f5cfeadd0
Yonatan Yatuv <yonatanyatuv@gmail.com>
2016-08-11 21:21:31 +0300

    repeat pipe
    Repeats a string n times
    Usage: string | repeat: n: separator[optional]

--- src/pipes/repeat.pipe.ts
+++ src/pipes/repeat.pipe.ts
@@ -1,3 +1,33 @@
 /**
- * Created by Yonatan on 8/11/2016.
+ * Created by Yonatan Yatuv on 11/August/2016.
  */
+
+import {PipeTransform, Pipe} from "@angular/core";
+
+@Pipe({
+    name: 'repeat'
+})
+
+export class RepeatPipe implements PipeTransform{
+    transform(value: string, ...args: any[]): any {
+        var times = args[0];
+        var separator = args[1];
+        return !times
+            ? value
+            : strRepeat(value, times-1, separator || '');
+    }
+}
+
+/**
+ * Repeats a string n times with given separator
+ * @param str string to repeat
+ * @param n number of times
+ * @param sep separator
+ * @returns {*}
+ */
+function strRepeat(str, n, sep) {
+    if(!n) {
+        return str;
+    }
+    return str + sep + strRepeat(str, --n, sep);
+}
